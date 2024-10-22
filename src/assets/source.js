9bc69305dd28dc0bf95b028d10e37eac8c9c5c3c
beshobassem707 <besho.bassem@transformus.co>
2024-10-22 17:15:34 +0300

    edit image source

--- src/assets/source.js
+++ src/assets/source.js
@@ -1,2 +1,2 @@
-// export const sourceBaseForImage = "./build/images"; // for server
-export const sourceBaseForImage = "./images";  // for local
+export const sourceBaseForImage = "./build/images"; // for server
+// export const sourceBaseForImage = "./images";  // for local
